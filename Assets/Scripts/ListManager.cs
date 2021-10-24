df9be34d67a170d5ecf112d700f593cadaa3c962
oscarburga <oscarburga2001@gmail.com>
2021-10-24 03:19:55 -0500

    experimentando creacion dinámica de elementos de UI

---
+++ Assets/Scripts/ListManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListManager : MonoBehaviour
+{
+
+    [SerializeField] private ArrayList AllListItems;
+	[SerializeField] private ArrayList AllListInstancedItems;
+    [SerializeField] private ArrayList SearchedListItems;
+    [SerializeField] private string CurrentSearch;
+    [SerializeField] private GameObject ContentPanel;
+    [SerializeField] private GameObject ListItemPrefab;
+
+    void Start()
+    {
+        // Deberia leerse la lista guardada del usuario
+        // Ejemplo usando solo strings
+        AllListItems = new ArrayList();
+        AllListInstancedItems = new ArrayList();
+        SearchedListItems = new ArrayList();
+    }
+
+    public void AddItem(string item)
+	{
+        if (!AllListItems.Contains(item)) {
+            AllListItems.Add(item);
+			GameObject newItem = Instantiate(ListItemPrefab) as GameObject;
+			ListItemManager lim = newItem.GetComponent<ListItemManager>();
+			if (lim == null)
+			{
+				Debug.Log(item + " gave null ListItemManager");
+			}
+			lim.text.text = item;
+			newItem.transform.parent = ContentPanel.transform;
+			newItem.transform.localScale = Vector3.one;
+			AllListInstancedItems.Add(newItem);
+		}
+	}
+
+
+    public void UpdateList()
+	{
+        if (CurrentSearch.Equals(""))
+		{
+			foreach(string item in AllListItems)
+			{
+				AddItem(item);
+			}
+		}
+	}
+
+
+}
