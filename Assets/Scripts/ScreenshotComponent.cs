651f47610ddedaacfb8065c353d55d4463c5219b
oscarburga <oscarburga2001@gmail.com>
2021-11-14 10:25:29 -0500

    Fix filter buttons not resetting on leaving and re-entering the filter screen

--- Assets/Scripts/ScreenshotComponent.cs
+++ Assets/Scripts/ScreenshotComponent.cs
@@ -20,20 +20,8 @@ public class ScreenshotComponent : MonoBehaviour
     private RawImage TakenPhotoRawImage = null;
     private string outputFolder;
 
-    public void ToggleModes(bool bActivatePendingSaveMode)
-	{
-        ConfirmPopupPanel.SetActive(false);
-        PendingSaveButtons.SetActive(bActivatePendingSaveMode);
-        TakenPhotoDisplay.SetActive(bActivatePendingSaveMode);
-        ActiveCameraButtons.SetActive(!bActivatePendingSaveMode);
-        CameraDisplay.SetActive(!bActivatePendingSaveMode);
-	}
-
-    public void ActivatePendingSaveMode() { ToggleModes(true); }
-    public void RestoreNormalMode() { ToggleModes(false); }
-    public void ShowConfirmationPopup() { ConfirmPopupPanel.SetActive(true); } 
-
-	public void Start()
+	#region Events
+	void Start()
 	{
         TakenPhotoRawImage = TakenPhotoDisplay.GetComponent<RawImage>();
         outputFolder = Application.persistentDataPath + "/Screenshots";
@@ -43,23 +31,26 @@ public class ScreenshotComponent : MonoBehaviour
             Debug.Log("Save Path will be : " + outputFolder);
         }
 	}
+	void OnEnable() { ToggleModes(false); }
+    #endregion
 
-	public void SavePendingToFile()
-    {
-        byte[] bytes;
-        bytes = TakenPhoto.EncodeToPNG();
-        string fileName = CreateFileName(screenshotWidth, screenshotHeight);
-        var file = File.Create(fileName);
-        file.Write(bytes, 0, bytes.Length);
-        file.Close();
-        Debug.Log(string.Format("Screenshot Saved {0}, size {1}", fileName, bytes.Length));
-    }
+    public void ToggleModes(bool bActivatePendingSaveMode)
+	{
+        ConfirmPopupPanel.SetActive(false);
+        PendingSaveButtons.SetActive(bActivatePendingSaveMode);
+        TakenPhotoDisplay.SetActive(bActivatePendingSaveMode);
+        ActiveCameraButtons.SetActive(!bActivatePendingSaveMode);
+        CameraDisplay.SetActive(!bActivatePendingSaveMode);
+	}
+    public void ActivatePendingSaveMode() { ToggleModes(true); }
+    public void RestoreNormalMode() { ToggleModes(false); }
+    public void ShowConfirmationPopup() { ConfirmPopupPanel.SetActive(true); } 
 
     public void TakePhoto()
 	{
         RenderTexture cameraRenderTexture = ARCamera.targetTexture;
 
-        // Save the camera view texture data
+        // Store the camera view texture data
         RenderTexture.active = cameraRenderTexture;
         TakenPhoto = new Texture2D(cameraRenderTexture.width, cameraRenderTexture.height, TextureFormat.RGB24, false);
         TakenPhoto.ReadPixels(new Rect(0, 0, cameraRenderTexture.width, cameraRenderTexture.height), 0, 0);
@@ -77,5 +68,16 @@ public class ScreenshotComponent : MonoBehaviour
 		var filename = string.Format("{0}/screen_{1}x{2}_{3}.{4}", outputFolder, width, height, timestamp, "png");
 		return filename;
 	}
+
+	public void SavePendingToFile()
+    {
+        byte[] bytes;
+        bytes = TakenPhoto.EncodeToPNG();
+        string fileName = CreateFileName(screenshotWidth, screenshotHeight);
+        var file = File.Create(fileName);
+        file.Write(bytes, 0, bytes.Length);
+        file.Close();
+        Debug.Log(string.Format("Screenshot Saved {0}, size {1}", fileName, bytes.Length));
+    }
 
 }
