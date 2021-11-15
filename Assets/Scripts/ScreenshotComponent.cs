3113b0c2a84a0272dfffd4f4d54dc7712e8e7f8f
Diego Livia Ortiz <diegolivia@hotmail.com>
2021-11-15 18:03:55 -0500

    Merge branch 'main' into feature/sprint-04_AR pt1

---
+++ Assets/Scripts/ScreenshotComponent.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenshotComponent : MonoBehaviour
+{
+    [SerializeField] public Camera ARCamera = null;
+    [SerializeField] public GameObject CameraDisplay = null;
+    [SerializeField] public GameObject TakenPhotoDisplay = null;
+    [SerializeField] public GameObject ConfirmPopupPanel = null;
+    [SerializeField] public GameObject ActiveCameraButtons = null;
+    [SerializeField] public GameObject PendingSaveButtons = null;
+    [SerializeField] public int screenshotWidth = 1920;
+    [SerializeField] public int screenshotHeight = 1080;
+
+    private Texture2D TakenPhoto = null;
+    private RawImage TakenPhotoRawImage = null;
+    private string outputFolder;
+
+	#region Events
+	void Start()
+	{
+        TakenPhotoRawImage = TakenPhotoDisplay.GetComponent<RawImage>();
+        outputFolder = Application.persistentDataPath + "/Screenshots";
+        if (!Directory.Exists(outputFolder))
+        {
+            Directory.CreateDirectory(outputFolder);
+            Debug.Log("Save Path will be : " + outputFolder);
+        }
+	}
+	void OnEnable() { ToggleModes(false); }
+    #endregion
+
+    public void ToggleModes(bool bActivatePendingSaveMode)
+	{
+        ConfirmPopupPanel.SetActive(false);
+        PendingSaveButtons.SetActive(bActivatePendingSaveMode);
+        TakenPhotoDisplay.SetActive(bActivatePendingSaveMode);
+        ActiveCameraButtons.SetActive(!bActivatePendingSaveMode);
+        CameraDisplay.SetActive(!bActivatePendingSaveMode);
+	}
+    public void ActivatePendingSaveMode() { ToggleModes(true); }
+    public void RestoreNormalMode() { ToggleModes(false); }
+    public void ShowConfirmationPopup() { ConfirmPopupPanel.SetActive(true); } 
+
+    public void TakePhoto()
+	{
+        RenderTexture cameraRenderTexture = ARCamera.targetTexture;
+
+        // Store the camera view texture data
+        RenderTexture.active = cameraRenderTexture;
+        TakenPhoto = new Texture2D(cameraRenderTexture.width, cameraRenderTexture.height, TextureFormat.RGB24, false);
+        TakenPhoto.ReadPixels(new Rect(0, 0, cameraRenderTexture.width, cameraRenderTexture.height), 0, 0);
+        RenderTexture.active = null;
+
+        // Capture the camera view into the TakenPhotoRawImage texture
+        ARCamera.targetTexture = TakenPhotoRawImage.texture as RenderTexture;
+        ARCamera.Render();
+        ARCamera.targetTexture = cameraRenderTexture;
+	}
+
+	private string CreateFileName(int width, int height)
+	{
+		string timestamp = DateTime.Now.ToString("yyyyMMddTHHmmss");
+		var filename = string.Format("{0}/screen_{1}x{2}_{3}.{4}", outputFolder, width, height, timestamp, "png");
+		return filename;
+	}
+
+	public void SavePendingToFile()
+    {
+        byte[] bytes;
+        bytes = TakenPhoto.EncodeToPNG();
+        string fileName = CreateFileName(screenshotWidth, screenshotHeight);
+        var file = File.Create(fileName);
+        file.Write(bytes, 0, bytes.Length);
+        file.Close();
+        Debug.Log(string.Format("Screenshot Saved {0}, size {1}", fileName, bytes.Length));
+    }
+
+}
--- Assets/Scripts/ShowFilterScreenButton.cs
+++ Assets/Scripts/ShowFilterScreenButton.cs
@@ -5,16 +5,32 @@ using UnityEngine.XR.ARFoundation;
 
 public class ShowFilterScreenButton : MonoBehaviour
 {
-    [SerializeField] ARFaceManager FaceManager;
-    [SerializeField] GameObject FacePrefab;
+    [SerializeField] public GameObject SelectFilterScreenRoot;
+    // [SerializeField] public string NextFilterButtonTag = "NextFilterButton";
+    [SerializeField] public NextFilterButtonComponent NextFilterButton;
+    [SerializeField] public GameObject FacePrefab;
 
-    public void SendPrefabToFaceManager()
+    public void SendPrefabData()
 	{
+        // Sadly, you can't find inactive game objects by tag, so scrap this idea for now and just set the NFB manually.
+
+      //  GameObject[] nextFilterButtons = GameObject.FindGameObjectsWithTag(NextFilterButtonTag);
+      //  if (FacePrefab && nextFilterButtons.Length > 0)
+	  //  {
+	  //  	ShowFilterScreenButton[] filterButtons = SelectFilterScreenRoot.GetComponentsInChildren<ShowFilterScreenButton>();
+      //      NextFilterButtonComponent nextFilterButton = nextFilterButtons[0].GetComponent<NextFilterButtonComponent>();
+	  //  	nextFilterButtons[0].GetComponent<NextFilterButtonComponent>().ReadFilterScreenButtonData(filterButtons, this);
+      //      if (nextFilterButton)
+      //          nextFilterButton.ReadFilterScreenButtonData(filterButtons, this);
+	  //  }
+      //  else Debug.Log(gameObject.name + ": error sending face prefabs data");
+
         if (FacePrefab)
-		{
-            FaceManager.facePrefab = FacePrefab;
-		}
-        else Debug.Log(gameObject.name + ": Button has null face prefab - can't send to face manager");
+	    {
+	    	ShowFilterScreenButton[] filterButtons = SelectFilterScreenRoot.GetComponentsInChildren<ShowFilterScreenButton>(true);
+            NextFilterButton.ReadFilterScreenButtonData(filterButtons, this);
+	    }
+        else Debug.Log(gameObject.name + ": error sending face prefabs data");
 	}
     
 }
