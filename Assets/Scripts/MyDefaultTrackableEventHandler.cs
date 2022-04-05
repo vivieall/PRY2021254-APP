9b9fcc503824486bbb054fd675fca2eb4fdb32c1
Hamill Cavero <gillecave500@gmail.com>
2022-04-04 21:21:03 -0500

    Semana 3
    Push durante presentación

---
+++ Assets/Scripts/MyDefaultTrackableEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MyDefaultTrackableEventHandler : DefaultTrackableEventHandler
+{
+    [SerializeField] public GameObject Canvas;
+    [SerializeField] public GameObject PointObj;
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        this.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        var canvasComponents = Canvas.GetComponentsInChildren<Canvas>(true);
+        foreach (var component in canvasComponents)
+            component.enabled = true;
+        for (int i = 0; i < PointObj.transform.childCount; i++)
+        {
+            PointObj.transform.GetChild(i).GetComponent<DragToUIObj>().SetAnswer(this.gameObject);
+        }
+        
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        var canvasComponents = Canvas.GetComponentsInChildren<Canvas>(false);
+        foreach (var component in canvasComponents)
+            component.enabled = false;
+    }
+}
